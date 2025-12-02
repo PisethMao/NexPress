fafa30d3c57ab1e0be2e9459510ff10fb7f601c0
YAN Group <yangroup@YANs-MacBook-Pro.local>
2025-12-02 15:38:45 +0700

    update login and signup

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import Login from "@/components/login/Login";
+import FooterSection from "@/components/FooterSection";
+
+export default function page() {
+  return (
+    <div>
+      <Login/> 
+      <FooterSection />
+    </div>
+  )
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,11 @@
+import SignUp from "@/components/signup/SignUp";
+import FooterSection from "@/components/FooterSection";
+
+export default function page() {
+  return (
+    <div>
+      <SignUp/> 
+      <FooterSection />
+    </div>
+  )
+}
